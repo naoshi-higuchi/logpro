4c4ad2d55ad2283830072b3a03ab988d010a5740
樋口 直志 <naoshi@Mac-mini.local>
2012-07-15 18:11:32 +0900

    Add initial files.

---
+++ logpro/src/main/java/jp/nhiguchi/libs/logpro/App.java
@@ -0,0 +1,13 @@
+package jp.nhiguchi.libs.logpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
